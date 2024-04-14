954ff992a3fb76d751ae78653fdb501160dfa56b
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2024-04-14 22:50:18 +0500

    Initial commit

---
+++ src/Person/Input.java
@@ -0,0 +1,11 @@
+package Person;
+
+import java.time.LocalDate;
+
+public class Operation extends Person{
+    public Operation(LocalDate birthday, String lastName, String firstName, String middleName, long phoneNumber, char gender) {
+        super(birthday, lastName, firstName, middleName, phoneNumber, gender);
+    }
+
+    private static Person 
+}
